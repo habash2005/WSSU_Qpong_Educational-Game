55b7e8fe57bf9ffe7d96980f2296fc2540360780
habash2005 <43829457+habash2005@users.noreply.github.com>
2024-01-28 13:18:20 -0500

    Add files via upload

---
+++ Quantum_Pong_Game/Quantum_Pong_Game/assets/ball.py
@@ -0,0 +1,42 @@
+from cmath import rect
+import pygame
+from . import globals
+import random
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        self.image = pygame.Surface([globals.WIDTH_UNIT, globals.WIDTH_UNIT])
+        self.image.fill(globals.WHITE)
+        self.rect = self.image.get_rect()
+        self.velocity = [1,2]
+        self.initial_speed = 2
+        self.reset(direction =-1)
+
+    def update(self, classical_computer, quantum_computer):
+        self.rect.x += self.velocity[0] #every second, 60 pixels
+        self.rect.y += self.velocity[1] #every second, 120 pixels
+
+        if self.rect.y < 0 or self.rect.y > globals.FIELD_HEIGHT - globals.WIDTH_UNIT: #above the screen, first half
+            self.velocity[1] = -self.velocity[1]
+
+        if self.rect.x < 0:
+            self.reset(1)
+            quantum_computer.score += 1
+        elif self.rect.x > globals.WINDOW_WIDTH:
+            self.reset(-1)
+            classical_computer.score += 1
+
+    def bounce(self):
+        self.velocity[0] = -self.velocity[0] * 1.5
+        self.velocity[1] = self.velocity[1] * 1.5
+
+    def reset(self, direction):
+        self.rect.centerx = globals.WINDOW_WIDTH // 2
+        self.rect.centery = globals.FIELD_HEIGHT // 2
+
+        if direction > 0:
+            self.velocity = [random.randint(2,4), random.randint(-4, 4)] * self.initial_speed
+        else:
+            self.velocity = [random.randint(-4,-2), random.randint(-4,4)] * self.initial_speed
---
+++ Quantum_Pong_Game/Quantum_Pong_Game/assets/ui.py
@@ -0,0 +1,86 @@
+from . import globals, resources
+import pygame
+
+def draw_statevector_grid(screen):
+    font = resources.Font()
+    basis_states = [
+        '|000>',
+        '|001>',
+        '|010>',
+        '|011>',
+        '|100>',
+        '|101>',
+        '|110>',
+        '|111>'
+    ]
+    statevector_height = int(round(globals.FIELD_HEIGHT / len(basis_states))) #8 basis states
+
+    for i in range(len(basis_states)):
+        text = font.vector_font.render(basis_states[i], 1, globals.RED)
+        screen.blit(text, (globals.WINDOW_WIDTH - text.get_width(),
+                            i*statevector_height + text.get_height()))
+
+def draw_score(screen, classical_score, quantum_score):
+    font = resources.Font()
+
+    text = font.player_font.render("Classical Computer", 1, globals.RED)
+    text_pos = text.get_rect(center = (globals.WINDOW_WIDTH*0.3, globals.WIDTH_UNIT*2))
+    screen.blit(text, text_pos)
+
+    text = font.score_font.render(str(classical_score), 1, globals.RED)
+    text_pos = text.get_rect(center=(globals.WINDOW_WIDTH*0.3, globals.WIDTH_UNIT*8))
+    screen.blit(text, text_pos)
+
+    text = font.player_font.render("Quantum Computer", 1, globals.RED)
+    text_pos = text.get_rect(center = (globals.WINDOW_WIDTH*0.7, globals.WIDTH_UNIT*2))
+    screen.blit(text, text_pos)
+
+    text = font.score_font.render(str(quantum_score), 1, globals.RED)
+    text_pos = text.get_rect(center=(globals.WINDOW_WIDTH*0.7, globals.WIDTH_UNIT*8))
+    screen.blit(text, text_pos)
+
+def draw_dashed_line(screen):
+    for i in range(10, globals.FIELD_HEIGHT, 2*globals.WIDTH_UNIT):
+        pygame.draw.rect(
+            screen,
+            globals.WHITE,
+            (globals.WINDOW_WIDTH // 2 - 5, i, 0.5 * globals.WIDTH_UNIT, globals.WIDTH_UNIT),
+            0,
+        )
+
+def draw_lose_scene(screen):
+    font = resources.Font()
+
+    
+    gameover_text = "Game Over"
+    text = font.gameover_font.render(gameover_text, 1, globals.RED)
+    text_pos = text.get_rect(center=(globals.WINDOW_WIDTH/2, globals.WIDTH_UNIT*10))
+    screen.blit(text, text_pos)
+
+    gameover_text = "Classical computer"
+    text = font.replay_font.render(gameover_text, 5, globals.RED)
+    text_pos = text.get_rect(center=(globals.WINDOW_WIDTH/2, globals.WIDTH_UNIT*22))
+    screen.blit(text, text_pos)
+
+    gameover_text = "still rules the world"
+    text = font.replay_font.render(gameover_text, 5, globals.RED)
+    text_pos = text.get_rect(center=(globals.WINDOW_WIDTH/2, globals.WIDTH_UNIT*27))
+    screen.blit(text, text_pos)
+
+def draw_win_scene(screen):
+    font = resources.Font()
+
+    gameover_text = "Congratulations!"
+    text = font.gameover_font.render(gameover_text, 5, globals.RED)
+    text_pos = text.get_rect(center=(globals.WINDOW_WIDTH/2, globals.WIDTH_UNIT*10))
+    screen.blit(text, text_pos)
+
+    gameover_text = "You demonstrated quantum advantage"
+    text = font.replay_font.render(gameover_text, 5, globals.RED)
+    text_pos = text.get_rect(center=(globals.WINDOW_WIDTH/2, globals.WIDTH_UNIT*22))
+    screen.blit(text, text_pos)
+
+    gameover_text = "for the first time in human history!"
+    text = font.replay_font.render(gameover_text, 5, globals.RED)
+    text_pos = text.get_rect(center=(globals.WINDOW_WIDTH/2, globals.WIDTH_UNIT*27))
+    screen.blit(text, text_pos)
