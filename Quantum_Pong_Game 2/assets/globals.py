248b4dabad8ef3411b948f7dbe48574119548983
habash2005 <ahmadhijaz325@gmail.com>
2024-01-28 17:52:46 -0500

    Paddles fixed

--- Quantum_Pong_Game 2/assets/globals.py
+++ Quantum_Pong_Game 2/assets/globals.py
@@ -13,8 +13,8 @@ NUM_QUBITS = 0
 WINDOW_WIDTH = 1200
 WINDOW_HEIGHT = 750
 FIELD_HEIGHT = round(WINDOW_HEIGHT * 0.7) # height of pong play field
-WIDTH_UNIT = round(WINDOW_WIDTH / 100) # width unit used for scaling the game
-PADDLE_HEIGHT = round(FIELD_HEIGHT / 2**NUM_QUBITS)
+WIDTH_UNIT = 12
+PADDLE_HEIGHT = 80
 
 # cool down time (in milliseconds) before the next measurement is allowed
 MEASUREMENT_COOLDOWN_TIME = 4000
